65682c8930eba91fa5d04cf1b11947d2b136e5e1
chuye <huangyuze.hyz@alibaba-inc.com>
2024-04-06 12:00:51 +0800

    add springboot-3.x samples

---
+++ spring-boot-3.x-samples/spring-boot-3.x-event-sample/src/main/java/com/hyz/spring/boot/event/EventPublishingRunListenerDemo.java
@@ -0,0 +1,12 @@
+package com.hyz.spring.boot.event;
+
+/**
+ * {@link org.springframework.boot.context.event.EventPublishingRunListener} demo
+ * @date 2024/4/6 11:15
+ * @since 1.0.0
+ */
+public class EventPublishingRunListenerDemo {
+    public static void main(String[] args) {
+
+    }
+}
